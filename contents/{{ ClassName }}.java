6654f08e5e9721cf159c9d52bea554b4e1894595
Jimmie Fulton <jimmie.fulton@gmail.com>
2021-06-16 22:21:10 -0700

    Reformat Java file

--- contents/{{ ClassName }}.java
+++ contents/{{ ClassName }}.java
@@ -1,17 +1,17 @@
 public class {{ ClassName }} {
-  private static {{ ClassName }} {{ className }} = new {{ ClassName }}();
+    private static {{ ClassName }} {{ className }} = new {{ ClassName }}();
 
-  private {{ ClassName }}(){}
+    private {{ ClassName }}(){}
 
-  public static {{ ClassName }} getInstance(){
-     return {{ className }};
-  }
+    public static {{ ClassName }} getInstance(){
+        return {{ className }};
+    }
 
-  public void printMessage() {
-      System.out.println("This is a contrived example. You can try this at home... but don't try it at work!");
-  }
+    public void printMessage() {
+        System.out.println("This is a contrived example. You can try this at home... but don't try it at work!");
+    }
 
-  public static void main(String[] args) {
-      {{ ClassName }}.getInstance().printMessage();
-  }
+    public static void main(String[] args) {
+        {{ ClassName }}.getInstance().printMessage();
+    }
 }
